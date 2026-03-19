f8345a317714656fc45ca1c72db7539de5fbfd82
wjz <wjz2930608106@qq.com>
2026-03-19 15:09:54 +0800

    docs: add pics for kenli

--- assets/js/members-data.js
+++ assets/js/members-data.js
@@ -22,7 +22,7 @@ const labMembers = {
             name: "Ken Li",
             title: "Ph.D. Student",
             period: "2026.09~Present",
-            photo: "./assets/img/people/liken.png",
+            photo: "./assets/img/people/kenli.png",
             homepage: "https://kiyotakali.github.io/",
             google_scholar: "https://scholar.google.com.hk/citations?hl=en&view_op=list_works&gmla=AGd7smF9375hTbVBKaAD7CX5RZYkAEByzOlP5dh5dLWzvGjsawOzO4m3gtLu7ZAoGplFQUekAfige1z77nwWH52r61yp&user=-wXqlyYAAAAJ",
             email: "2223312154@stu.xjtu.edu.cn",
