562d6b07d63ecec1660a2fbfc1db09c85cf7dcbf
wjz <wjz2930608106@qq.com>
2026-03-19 19:19:14 +0800

    docs:add members xinquan

--- assets/js/members-data.js
+++ assets/js/members-data.js
@@ -63,6 +63,15 @@ const labMembers = {
             google_scholar: "https://scholar.google.com/citations?user=UgKRQJwAAAAJ&hl=en&oi=ao",
             email: "jzw6545@gmail.com",
             github: "https://github.com/JiazheWei"
+        },
+{
+            name: "Xinquan Yue",
+            title: "Master's Student",
+            period: "2026.09~Present",
+            photo: "./assets/img/people/xinquanyue.jpg",
+            google_scholar: "https://scholar.google.com/citations?hl=zh-CN&user=t1Y9whQAAAAJ",
+            email: "221900005@smail.nju.edu.cn",
+            github: "https://github.com/windchaser170721"
         }
     ],
     
---
+++ people/xinquanyue.md
@@ -0,0 +1,11 @@
+---
+name: Xinquan Yue
+title: Master's Student
+period: 2026.09~Present
+photo: ./assets/img/people/xinquanyue.jpg
+google_scholar: https://scholar.google.com/citations?hl=zh-CN&user=t1Y9whQAAAAJ 
+email: 221900005@smail.nju.edu.cn
+github: https://github.com/windchaser170721
+order: 2
+---
+
