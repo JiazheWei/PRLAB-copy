9080fb4240c9584488697ccc0786ce29bb8a9d05
wjz <wjz2930608106@qq.com>
2026-03-19 17:20:42 +0800

    docs:add member meikai li

--- assets/js/members-data.js
+++ assets/js/members-data.js
@@ -43,6 +43,13 @@ const labMembers = {
             period: "2026.09~Present",
             photo: "./assets/img/people/zeyulou.png",
             email: "zeyu.lou.mail@gmail.com"
+        },
+{
+            name: "Meikai Li",
+            title: "Ph.D. Student",
+            period: "2026.09~Present",
+            photo: "./assets/img/people/meikaili.jpg",
+            email: "meikaili@smail.nju.edu.cn"
         }
     ],
     
---
+++ people/meikaili.md
@@ -0,0 +1,9 @@
+---
+name: Meikai Li
+title: Ph.D. Student
+period: 2026.09~Present
+photo: ./assets/img/people/meikaili.jpg
+email: meikaili@smail.nju.edu.cn
+order: 4
+---
+
